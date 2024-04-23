c1fde3a6c827331442e572ff726f4d80ff9ff7d0
Ron <ron@lisles.net>
2024-04-23 07:00:25 -0500

    wip

--- Photon/Controllers/FrontPanel/src/FrontPanel.ino
+++ Photon/Controllers/FrontPanel/src/FrontPanel.ino
@@ -123,16 +123,44 @@
  
  */
 #include <IoT.h>
-#include "Settings.h"
-#include "Behaviors.h"
-#include "EventHandlers.h"
 
+#define CONTROLLER_NAME "FrontPanel"
+#define MQTT_BROKER "192.168.0.33"
+#define PCA9685_ADDRESS 0x41       // Lowest jumper set
+//#define SWITCH_ADDRESS 0x20
+//#define SWITCH_IOMAP 0xFF       // All 8 GPIOs are inputs
+
+int const frontDoorLightTimeout = 15*1000;
+int const livingRoomMotionTimeout = 3*1000;
+int const coffeeMotionTimeout = 3*1000;
+
+#define CURVE 2 // 0 = Linear, 1 = exponential, 2 = 50/50
+
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(AUTOMATIC);
+
+// Write Log messages to MQTT and/or serial port
+#define MQTT_LOGGING true
+//SerialLogHandler logHandler1(57600, LOG_LEVEL_INFO);
+
+// Global Variables
+int voltage = 0;
+bool isTimingLivingRoomMotion = false;
+bool isTimingFrontDoor = false;
+bool isTimingCoffeeMotion = false;
+
+
+// LOOP
 void loop() {
     IoT::loop();
 }
 
+
+// SETUP
 void setup() {
-    setupWifi();    // in Settings.h
+//    WiFi.setCredentials(WIFI_SSID, WIFI_PASSWORD);
+//    WiFi.selectAntenna(ANT_INTERNAL);
+//    WiFi.useDynamicIP();
     
     // This also sets timezone and DST
     IoT::begin(MQTT_BROKER, CONTROLLER_NAME, MQTT_LOGGING);
@@ -140,27 +168,23 @@ void setup() {
     // Required by NCD8Light
     PCA9685::initialize(PCA9685_ADDRESS);
 
-    // Behaviors
-    setNextMinuteHandler(handleNextMinute);
-    Device::setAnyChangedHandler(updateLights);
-
     // Be careful that names don't collide with Homebridge (commands)
-    Device::add(new Device("AnyoneHome", "Status", 'S'));
-    Device::add(new Device("Bedroom", "Status", 'L'));
-    Device::add(new Device("Cabinets", "Status", 'S'));
-    Device::add(new Device("Cleaning", "Status", 'S'));
-    Device::add(new Device("Kitchen", "Status", 'L'));
-    Device::add(new Device("LivingRoomMotion", "Status", 'S', handleLivingRoomMotion));
-    Device::add(new Device("Nighttime", "Status", 'S'));
-    Device::add(new Device("Nook", "Status", 'L'));
-    Device::add(new Device("OfficeMotion", "Status", 'S'));
-    Device::add(new Device("Outside", "Status", 'S'));
-    Device::add(new Device("Retiring", "Status", 'S', handleRetiring));
-    Device::add(new Device("RonHome", "Status", 'S'));
-    Device::add(new Device("ShelleyHome", "Status", 'S'));
-    Device::add(new Device("Sink", "Status", 'L'));
-    Device::add(new Device("Sleeping", "Status", 'S', handleSleeping));
-    Device::add(new Device("Theatre", "Status", 'S'));
+    // Device::add(new Device("AnyoneHome", "Status", 'S'));
+    // Device::add(new Device("Bedroom", "Status", 'L'));
+    // Device::add(new Device("Cabinets", "Status", 'S'));
+    // Device::add(new Device("Cleaning", "Status", 'S'));
+    // Device::add(new Device("Kitchen", "Status", 'L'));
+    // Device::add(new Device("LivingRoomMotion", "Status", 'S', handleLivingRoomMotion));
+    // Device::add(new Device("Nighttime", "Status", 'S'));
+    // Device::add(new Device("Nook", "Status", 'L'));
+    // Device::add(new Device("OfficeMotion", "Status", 'S'));
+    // Device::add(new Device("Outside", "Status", 'S'));
+    // Device::add(new Device("Retiring", "Status", 'S', handleRetiring));
+    // Device::add(new Device("RonHome", "Status", 'S'));
+    // Device::add(new Device("ShelleyHome", "Status", 'S'));
+    // Device::add(new Device("Sink", "Status", 'L'));
+    // Device::add(new Device("Sleeping", "Status", 'S', handleSleeping));
+    // Device::add(new Device("Theatre", "Status", 'S'));
 
     // Inside Lights    TODO: set actual light #s     TODO: set wire colors
     Device::add(new NCD16Light(15, "KitchenCeiling", "Kitchen",0));   // R
@@ -184,8 +208,8 @@ void setup() {
     Device::add(new NCD16Light(4, "FrontPorch", "Outside",0));        // W
 
     // Zigbee Motion Detectors & Contact Switches
-    Device::add(new ZigbeeMotion("CoffeeMotion", "Office", handleCoffeeMotion));
-    Device::add(new ZigbeeContact("FrontDoor", "Living Room", handleFrontDoor));
+    Device::add(new ZigbeeMotion("CoffeeMotion", "Office"));
+    Device::add(new ZigbeeContact("FrontDoor", "Living Room"));
 
     // 12V Monitor (actually 14.27) with 10:1 R-Ladder
     // Adjust fullScale to reflect actual R-Ladder (36.9)
